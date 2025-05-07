269fde11d730af43580a25d825d717792b0e0ffc
Shahitthiyan Murali <shaketthiyanmurali@gmail.com>
2025-05-07 19:14:04 +0530

    Update (SEU.IS.22.ICT.052) login page.c

--- (SEU.IS.22.ICT.052) login page.c
+++ (SEU.IS.22.ICT.052) login page.c
@@ -3,24 +3,33 @@
 
 int login() {
     char username[8], password[10];
+    int attempts = 3;
 
-    printf("Enter username: ");
-    scanf("%8s", username); // in hear we type the u.n as 8s it's indicate to Prevents buffer overflow by limiting input length (scanf("%8s", username))
+    while (attempts > 0) {
+        printf("\n=======Login page=======\n");
+        printf("Enter username: ");
+        scanf("%8s", username);
 
+        printf("Enter password: ");
+        scanf("%10s", password);
 
-    printf("Enter password: ");
-    scanf("%10s", password);
-
-    if (strcmp(username, "admin") == 0 && strcmp(password, "admin123") == 0) {  //in hear strcmp is used to compare two strings
-        printf("Login successful.\n");
-        return 1;
-    } else {
-        printf("Invalid credentials.\n");
-        return 0;
+        if (strcmp(username, "admin") == 0 && strcmp(password, "admin123") == 0) {
+            printf("Login successful.\n");
+            return 1;
+        } else {
+            attempts--;
+            if (attempts > 0) {
+                printf("Invalid credentials. %d attempts remaining\n", attempts);
+            } else {
+                printf("Invalid credentials. No attempts remaining. Login failed.\n");
+            }
+        }
     }
+    return 0;
 }
 
 int main() {
     login();
     return 0;
 }
+
